7312e13c94ca03973f7363b772026e31b9222421
Matheus Felipe <matheus495486@gmail.com>
2024-09-25 12:48:39 -0300

    The post method, from JogadorController was made

--- src/main/java/dev/web/mensalidades/controller/JogadorController.java
+++ src/main/java/dev/web/mensalidades/controller/JogadorController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,15 @@ public class JogadorController {
 
     }
 
+    @PostMapping("/")
+    public ResponseEntity<Jogador> createJogador(@RequestBody Jogador jogador) {
+        try {
+            Jogador j = jog.save(new Jogador(jogador.getNome(), jogador.getEmail(), jogador.getDatanasc()));
+            return new ResponseEntity<>(j, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }    
 
 }
